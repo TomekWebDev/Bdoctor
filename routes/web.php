0eaaf8733a96eb94632bdc80f8a7f84c3df50409
MarcoPerta <perta.marco@gmail.com>
2023-02-16 17:59:22 +0100

    home controller

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin') //localhost:8080/admin/posts
+    ->name('admin.')
+    ->group(function(){
+
+        Route::get('/', 'HomeController@index')->name('index');
+
+    });
+
