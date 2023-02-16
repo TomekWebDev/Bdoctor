8635697a358e79708637429c5d6b6de67301aade
Elsti97 <elsti.shehati@hotmail.it>
2023-02-16 10:09:18 +0100

    commit base

--- public/js/app.js
+++ public/js/app.js
@@ -49813,8 +49813,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/Mangiagalli/Desktop/Progetto finale/Bdoctor/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/Mangiagalli/Desktop/Progetto finale/Bdoctor/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Boolean77\progettoFinale\Bdoctor\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Boolean77\progettoFinale\Bdoctor\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
