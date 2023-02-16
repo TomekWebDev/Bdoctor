1097529466648b958ddd5c07881e565c56f015b5
TomekWebDev <tommaso.mangiagalli@gmail.com>
2023-02-16 12:02:45 +0100

    Created models and described relational functions

---
+++ app/Models/Profile.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany('App\Models\Message');
+    }
+
+    public function sponsors()
+    {
+        return $this->belongsToMany('App\Models\Sponsor');
+    }
+
+    public function specs()
+    {
+        return $this->belongsToMany('App\Models\Spec');
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany('App\Models\Review');
+    }
+
+    public function ratings()
+    {
+        return $this->belongsToMany('App\Models\Rating');
+    }
+}
