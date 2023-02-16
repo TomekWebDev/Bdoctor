9203c61ca9fb14cab0f274c29f67fa260c9f6387
mecarm <melecarmine99@gmail.com>
2023-02-16 12:21:56 +0100

    table profiles

---
+++ database/migrations/2023_02_16_111012_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('city', 80);
+            $table->string('address', 100);
+            $table->text('resume')->nullable();
+            $table->string('image')->nullable();
+            $table->string('phone', 13)->nullable();
+            $table->text('description')->nullable();
+            $table->text('services')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
