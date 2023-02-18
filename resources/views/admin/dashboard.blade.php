a93f8780e96eb3ceb91cae1e55ca1ae95d784cc4
mecarm <melecarmine99@gmail.com>
2023-02-18 16:03:30 +0100

    Fix layout iniziale dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,8 +3,42 @@
 
 
 @section('content')
-    <div>
-        <h4 class="mb-5">CIAOOOOOOOOOOOO Dashboard dello user: {{ $user->name }} con id: {{ $user->id }}</h4>
+   
+    <div class="container">
+        <div class="card">
+            <div class="d-flex">
+                <div class="card-body">
+                    <h1>Dashboard <small class="text-muted">{{$user->name}}  {{$user->surname}} con id {{$user->id}}</small></h1>
+                </div>
+
+                <div class="dropdown align-self-center mr-3">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
+                        Action
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li> 
+                        <a class="dropdown-item" href="{{route('admin.profile.edit', $profile->id)}}">Modifica Profilo</a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="{{route('admin.profile.show', $profile->id)}}">Il mio profilo</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{route('admin.messages.index', $profile->id)}}">Messaggi ricevuti</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{route('admin.reviews.index', $profile->id)}}">Le mie Recensioni</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{route('admin.sponsors.index', $profile->id)}}">Aggiungi Sponsor</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{route('admin.statistics.index', $profile->id)}}">Statistiche</a>
+                    </li>
+                    </ul>
+                  </div>
+            </div>
+            
+        </div>
     </div>
     {{-- @if (session('alreadyCreated'))
         <div>{{ session('alreadyCreated') }}</div>
@@ -15,23 +49,22 @@
     @endif --}}
 
     {{-- da togliere --}}
-    <a href="{{ route('admin.profile.create') }}">vai a create</a>
+    {{-- <a href="{{ route('admin.profile.create') }}">vai a create</a> --}}
 
+    <div class="container text-center mt-5 border rounded-pill " style="box-shadow: 0px 0px 10px 0px rgb(80, 80, 80)">
+        <div class="row">
+            <div class="col-12 p-3">
+                <h4 class="fs-3 text-capitalize fw-bold">{{$user->name}} {{$user->surname}}</h4>
+                <div class="div d-flex flex-wrap mt-2 justify-content-center">
+                    @foreach ($profile->specs as $spec)
+                        <span class="badge bg-primary m-1">{{ $spec->name }}</span>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+    
 
-    <a href="{{ route('admin.profile.edit', $profile->id) }}">Vai a edit</a>
-
-    <a href="{{ route('admin.profile.show', $profile->id) }}">Vai alla show</a>
-
-    <a href="{{ route('admin.messages.index', $profile->id) }}">Vai ai messaggi</a>
-
-    <a href="{{ route('admin.reviews.index', $profile->id) }}">Vai alle reviews</a>
-
-    <a href="{{ route('admin.ratings.index', $profile->id) }}">Vai ai ratings</a>
-
-    <a href="{{ route('admin.sponsors.index', $profile->id) }}">Scegli una sponsorizzazione</a>
-
-    <a href="{{ route('admin.statistics.index', $profile->id) }}">Vai alle tue statistiche</a>
-
-
-    <a href="">Vai ad acquisto sponsor</a>
 @endsection
+
+
