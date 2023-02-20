575465905b008b6ae1306a7637da13bc98538dbd
Elsti97 <elsti.shehati@hotmail.it>
2023-02-20 17:40:00 +0100

    Update show WIP

--- resources/views/admin/profile/show.blade.php
+++ resources/views/admin/profile/show.blade.php
@@ -1,11 +1,68 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>questa è la show del dottore</div>
+    <div class="container">
+        <div>questa è la show del dottore</div>
 
-    <div>{{ $this_user->name }}</div>
-    <div>{{ $this_user->surname }}</div>
-    <div>{{ $this_profile->city }}</div>
+        <div class="card">
+            <div class="card-body">
+                <div>{{ $this_user->name }}</div>
+                <div>{{ $this_user->surname }}</div>
+                <div>{{ $this_profile->city }}</div>
+                <div>{{ $this_profile->address }}</div>
+                <img src="{{ asset("Storage/$this_profile->image") }}" class="img-fluid w-25">
 
-    <a href="{{ route('admin.profile.edit', $this_profile->id) }}">Vai a edit</a>
+                {{-- <embed src="{{ asset("Storage/$this_profile->resume") }}" class="img-fluid w-25" type='application/pdf'/> --}}
+
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" id="myInput">
+                    Bottone CV
+                </button>
+
+            </div>
+
+        </div>
+
+
+        <a href="{{ route('admin.profile.edit', $this_profile->id) }}">Vai a edit</a>
+    </div>
+
+    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    {{-- <embed src="{{ asset("Storage/$this_profile->resume") }}" class="img-fluid w-25" type='application/pdf'/> --}}
+                        Ciao
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+
+{{-- @push('script')
+    <script>
+        const myModal = document.getElementById('myModal');
+        const myInput = document.getElementById('myInput');
+
+        myModal.addEventListener('shown.bs.modal', () => {
+            myInput.focus()
+        })
+    </script>
+@endpush --}}
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('#myInput').click(function() {
+                $('#myModal').modal('show');
+            });
+        });
+    </script>
+@endpush
