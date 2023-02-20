1076f267058b9817b6cce34e279ac4e7a46a6cdd
Elsti97 <elsti.shehati@hotmail.it>
2023-02-20 19:38:38 +0100

    added no messages/reviews

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -7,7 +7,7 @@
 
                 {{-- {{dd($messages)}} --}}
 
-                @foreach ($messages as $singleMessage)
+                {{-- @foreach ($messages as $singleMessage)
                     <ul>
                         <li>
 
@@ -15,7 +15,22 @@
                             puoi risponderli scivendo a: {{$singleMessage->email}}
                         </li>
                     </ul>
-                @endforeach
+                @endforeach --}}
+
+                @if (count($messages) > 0)
+                    @foreach ($messages as $singleMessage)
+                        <ul>
+                            <li>
+
+                                {{ $singleMessage->surname }} {{ $singleMessage->name }} ti domanda:
+                                {{ $singleMessage->message }},
+                                puoi risponderli scivendo a: {{ $singleMessage->email }}
+                            </li>
+                        </ul>
+                    @endforeach
+                @else
+                    <h2 class="text-center">Non hai nessun messaggio</h2>
+                @endif
 
             </div>
         </div>
--- resources/views/admin/reviews/index.blade.php
+++ resources/views/admin/reviews/index.blade.php
@@ -6,13 +6,18 @@
             <div class="card-body">
                 <h1>Reviews <small class="text-secondary">{{ $profile->name }}</small></h1>
                 {{-- Qua sotto va fatto foreach --}}
-                @foreach ($reviews as $rev)
-                    <ul>
-                        <li>
-                            Recensione di {{ $rev->name }}: {{ $rev->review }}
-                        </li>
-                    </ul>
-                @endforeach
+
+                @if (count($reviews) > 0)
+                    @foreach ($reviews as $rev)
+                        <ul>
+                            <li>
+                                Recensione di {{ $rev->name }}: {{ $rev->review }}
+                            </li>
+                        </ul>
+                    @endforeach
+                @else
+                    <h2 class="text-center">Non hai ancora nessuna recensione</h2>
+                @endif
 
                 <br>
 
