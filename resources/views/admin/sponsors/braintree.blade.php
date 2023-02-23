24ec0068951f25c9e5b2cc29e50b05703aeb05a7
Elsti97 <elsti.shehati@hotmail.it>
2023-02-23 18:50:03 +0100

    fixed title

--- resources/views/admin/sponsors/braintree.blade.php
+++ resources/views/admin/sponsors/braintree.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Donato suca</title>
+    <title>Pagamento</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 </head>
@@ -25,9 +25,9 @@
 
 <div class="w-50 mx-auto">
     <form method="POST" id="payment-form" action="{{ route('admin.sponsors.checkout', $type->price) }}">
-        
+
         @csrf
-        <h1 class="text-center">Donato suca <span>{{$type->name}}</span></h1>
+        <h1 class="text-center">Hai scelto la versione: <span>{{$type->name}}</span></h1>
         <section class="mt-6 mb-3">
             <label class="d-flex">
                 <span>Prezzo:</span>
